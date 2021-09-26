3038dc6a6bfa0ff6a8b10b8cc7bd44cfe94c1f7e
scirelli <scirelli+git@gmail.com>
2021-09-25 21:22:13 -0400

    Created a strings utils library. issue #42

---
+++ internal/pkg/stringUtils/strings.go
@@ -0,0 +1,35 @@
+package stringutils
+
+import (
+	"regexp"
+	"strings"
+)
+
+var matchPunctuation = regexp.MustCompile(`[[:punct:]]`)
+
+func FilterEmpty(s []string) []string {
+}
+
+func SliceToDict(s []string) map[string]struct{} {
+	dict := make(map[string]struct{})
+	for _, w := range s {
+		dict[w] = struct{}{}
+	}
+	return dict
+}
+
+func ToLower(s []string) []string {
+	o := make([]string, len(s))
+	for i, w := range s {
+		o[i] = strings.ToLower(w)
+	}
+	return o
+}
+
+func StripPunctuation(s []string) []string {
+	o := make([]string, len(s))
+	for i, w := range s {
+		o[i] = string(matchPunctuation.ReplaceAll([]byte(w), []byte("")))
+	}
+	return o
+}
