11e3865b3a11de0a5447697142655b3b82f6e07b
scirelli <scirelli+git@gmail.com>
2021-09-25 21:22:13 -0400

    Created a strings utils library. issue #42

--- internal/pkg/ebidlocal/filter/AuctionItem.go
+++ internal/pkg/ebidlocal/filter/AuctionItem.go
@@ -1,1 +1,18 @@
 package filter
+
+import (
+	"strings"
+
+	"github.com/scirelli/auction-ebidlocal-search/internal/pkg/ebidlocal/model"
+	stringutils "github.com/scirelli/auction-ebidlocal-search/internal/pkg/stringUtils"
+)
+
+func ByKeyword(item model.AuctionItem) bool {
+	keywordLookup := stringutils.SliceToDict(stringutils.ToLower(item.Keywords))
+	for _, f := range stringutils.ToLower(stringutils.StripPunctuation(strings.Fields(item.String()))) {
+		if _, exists := keywordLookup[f]; exists {
+			return true
+		}
+	}
+	return false
+}
