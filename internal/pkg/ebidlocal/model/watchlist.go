2c7a7835921ed82261968d7a268bfdd3dfc45d2c
scirelli <scirelli+git@gmail.com>
2021-10-05 20:42:30 -0400

    Issue #50: Removed duplicate keys from watch lists

--- internal/pkg/ebidlocal/model/watchlist.go
+++ internal/pkg/ebidlocal/model/watchlist.go
@@ -1,6 +1,9 @@
 package model
 
 import (
+	"sort"
+	"strings"
+
 	"github.com/scirelli/auction-ebidlocal-search/internal/pkg/id"
 	"github.com/scirelli/auction-ebidlocal-search/internal/pkg/iter/stringiter"
 )
@@ -13,4 +16,22 @@ func (w Watchlist) ID() string {
 
 func (w Watchlist) Iterator() stringiter.Iterator {
 	return stringiter.SliceStringIterator(w).Iterator()
+}
+
+func (w *Watchlist) Normalize() *Watchlist {
+	set := make(map[string]struct{})
+	for _, s := range *w {
+		set[s] = struct{}{}
+	}
+
+	i := 0
+	tmp := Watchlist(make([]string, len(set)))
+	for key := range set {
+		tmp[i] = strings.ToLower(key)
+		i++
+	}
+
+	*w = tmp
+	sort.Strings(*w)
+	return w
 }
